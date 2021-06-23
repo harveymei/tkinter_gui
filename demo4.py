cd9811388272a7ea86ff4b8a5c3ca15337d1d850
Harvey Mei <harvey.mei@msn.com>
2021-06-23 21:48:39 +0800

    Commit Message

--- demo4.py
+++ demo4.py
@@ -6,3 +6,14 @@
 # @IDE     : PyCharm
 # @GitHub  : https://github.com/harveymei/
 
+from tkinter import *
+
+
+win = Tk()
+win.title("标题")
+win.geometry("300x150")
+win.configure(bg="yellow")
+win.maxsize(500, 500)
+couple = "\n\n上联：足不出户一台电脑打天下\n\n下联：窝宅在家一双巧手定乾坤\n\n横批：真我风采"
+txt = Label(win, text=couple, bg="yellow").pack()
+win.mainloop()
