0f2bd34ee0b851b561aadb11aed0dcdcbd09adc9
Harvey Mei <harvey.mei@msn.com>
2021-06-24 10:03:34 +0800

    Commit Message

--- demo4.py
+++ demo4.py
@@ -25,10 +25,11 @@ win.geometry("300x150")
 win.configure(bg="yellow")
 # 设置窗口的最大尺寸（即最大化按钮或拖拉水平和垂直边框所能得到的最大窗口）
 win.maxsize(500, 500)
-# 定义字符串变量
+# 定义字符串变量（9行）
 couple = "\n\n上联：足不出户一台电脑打天下\n\n下联：窝宅在家一双巧手定乾坤\n\n横批：真我风采"
-# 设置Label赋值变量并包装
+# 设置Label赋值变量并包装显示
 # 此处的背景色为标签的背景色，如不设置，默认为白色。
 txt = Label(win, text=couple, bg="yellow")
 txt.pack()
+
 win.mainloop()
---
+++ demo5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time    : 2021/6/24 10:02
+# @Author  : Harvey Mei <harvey.mei@msn.com>
+# @FileName: demo5.py
+# @IDE     : PyCharm
+# @GitHub  : https://github.com/harveymei/
+
+"""
+设置窗口大小及位置
+"""
+
