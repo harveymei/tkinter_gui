766102b9a6b69f779a78e0abde5f01e3f0083988
Harvey Mei <harvey.mei@msn.com>
2021-06-24 09:31:35 +0800

    Commit Message

--- demo4.py
+++ demo4.py
@@ -6,12 +6,19 @@
 # @IDE     : PyCharm
 # @GitHub  : https://github.com/harveymei/
 
+"""
+设置窗口样式
+"""
+# 导入模块
 from tkinter import *
 
-
+# 实例化窗口
 win = Tk()
+# 设置标题
 win.title("标题")
+# 设置窗口宽度与高度，中间字符位字母"x"，单位为像素
 win.geometry("300x150")
+#
 win.configure(bg="yellow")
 win.maxsize(500, 500)
 couple = "\n\n上联：足不出户一台电脑打天下\n\n下联：窝宅在家一双巧手定乾坤\n\n横批：真我风采"
