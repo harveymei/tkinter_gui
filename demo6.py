de8fcc319f74e18e65f7913ecc732267cfc1a187
Harvey Mei <harvey.mei@msn.com>
2021-07-20 15:04:04 +0800

    Commit Message

---
+++ demo6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time    : 2021/6/24 11:48
+# @Author  : Harvey Mei <harvey.mei@msn.com>
+# @FileName: demo6.py
+# @IDE     : PyCharm
+# @GitHub  : https://github.com/harveymei/
+
+from tkinter import *
+
+win = Tk()
+win.geometry()
+Label()
+win.mainloop()
