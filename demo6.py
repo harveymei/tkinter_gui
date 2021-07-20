815e3681981fab7b2f6990d7a0716c661b27580b
Harvey Mei <harvey.mei@msn.com>
2021-07-20 15:23:43 +0800

    Commit Message

--- demo6.py
+++ demo6.py
@@ -6,9 +6,14 @@
 # @IDE     : PyCharm
 # @GitHub  : https://github.com/harveymei/
 
+"""
+指定窗口大小及文字样式
+"""
 from tkinter import *
 
 win = Tk()
-win.geometry()
-Label()
+win.geometry("300x200")
+# Label(win, text="小扣柴扉久不开", foreground="red", background="#C3DEEF").pack()
+# 设置组建的宽度与高度，单位为行
+Label(win, text="小扣柴扉久不开", fg="red", bg="#C3DEEF", width=20, height=3).pack()
 win.mainloop()
